8620b0e94ea1a3d6b256c1022bcee501f8469951
poncho29 <meneses0828@gmail.com>
2023-04-01 10:21:12 -0500

    request of products and categories

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,29 @@ import Head from 'next/head'
 import { Heading } from '@chakra-ui/react'
 import { Button } from '@chakra-ui/react'
 import { Header } from '@/components/Header'
+import { GetServerSidePropsContext } from 'next'
 
-export default function Home() {
+type Product = {
+  id: number;
+  title: string;
+  price: string;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    count: number;
+    rate: number;
+  }
+}
+
+type Category = "electronics" | "jewelery" | "men's clothing" | "women's clothing";
+
+type Props = {
+  products: Product[];
+  categories: Category;
+}
+
+export default function Home({ products, categories }: Props) {
   return (
     <>
       <Head>
@@ -16,10 +37,20 @@ export default function Home() {
       <Header />
 
       <main>
-        <Heading>Hello World</Heading>
-
-        <Button/>
+       
       </main>
     </>
   )
+}
+
+export async function getServerSideProps(contex: GetServerSidePropsContext) {
+  const products = await fetch('https://fakestoreapi.com/products').then(res => res.json())
+  const categories = fetch('https://fakestoreapi.com/products/categories').then(res=>res.json())
+
+  return {
+    props: {
+      products,
+      categories
+    }
+  }
 }
